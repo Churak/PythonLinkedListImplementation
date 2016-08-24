84459ac40d1e855186eaf7a62637665a0eaba549
Churak <jfr7294@gmail.com>
2016-08-23 21:09:31 -0400

    Finished Linked list implementation and docus
    Added get, remove, size, and add functions to the program.
    Finished documentation and pseudo code related to the method implmentation.
    Added __init__.py so importing the python files and the classes could be
    completed from the python console.

--- README.md
+++ README.md
@@ -50,6 +50,25 @@ class LinkedList
         * n+1  
       * endloop  
 
+  * remove()
+    * start at the root node
+    * check if the rootNode.Next is empty
+      * If yes, list is empty
+      * If not, adjust the removeNode to the current removeNode.Next and create a preremoveNode track the node that will have the next point removed.
+        * while RemoveNode is not the last node loop
+          * set the RemoveNode and PreRemoveNode
+        * Once the loop breaks, we're at the end.
+        * Set PreRemoveNode.Next to null to remove the last node
+
+  * getNode(index)
+    * start at the root node
+    * If the list isn't currently empty, set GetNode to rootNode.Next
+    * For loop with user set index
+      * Set GetNode as GetNode.Next
+      * Check if the current node is the last in the list, if it is, break and return
+    * Endloop
+
+    GetNode is now the desired node, return GetNode.Data
 
 #### Example Usage  
 list = LinkedList()  
--- linkedlist.py
+++ linkedlist.py
@@ -2,11 +2,91 @@
 # Single Linkedlist implemenation in Python while not
 # using array or linked list structures.
 
-
 # Node class which defines what is a node and the attributes relating
-# to both.
-
 class Node:
-    Next = None
-    Data = None
+    def __init__(self, NextNode=None, NodeData=None):
+        self.Data = NodeData
+        self.Next = NextNode
+
+
+class LinkedList:
+    def __init__(self):
+        self.rootNode = Node()
+
+    def add(self, object):
+        # Set the reference for the system, all adds will be in relation to this
+        # root node with the new object being added to the end of the list.
+        LastNode = self.rootNode
+        NewNode = Node(None, object);
+
+        # Iterate through the linkedlist. While the Node.Next attribute is not
+        # null (meaning it's not the last in the list) keep iterating through
+        while LastNode.Next != None:
+            LastNode = LastNode.Next
+
+        LastNode.Next = NewNode
+
+        return
+
+    def size(self):
+        # Set the reference for the method, counts must happen in relation to the
+        # root node, just as it was with the add method
+        CountNode = self.rootNode
+        size = 0
+
+        # While the node.next is pointing to another node, iterate through the list
+        # keeping track of the number of jumps made.
+        while CountNode.Next != None:
+            CountNode = CountNode.Next
+            size = size + 1
+
+        return size
+
+    def remove(self):
+        # Set the rootNode as reference.
+        # PreRemoveNode is set to track the n-1 node in the list. This node will become
+        # the new last node in the sequence, so we need to track it so we can wipe it's
+        # pointer to the old node
+        PreRemoveNode = self.rootNode
+        
+        # Checks if the root node has a next pointer, if it doesn't,
+        # return that the list is empty
+        if PreRemoveNode.Next == None:
+            return
+
+        # Knowing that that list is not empty, we know that the next node in line
+        # is possibly the one getting removed. Set up the remove node to represent tge
+        # node that will be removed.
+        RemoveNode = PreRemoveNode.Next
+
+        # Check if the remove node pointer is empty. It it's not empty, we have a new 
+        # set of nodes that are our preremove and remove node, so set them as such.
+        while RemoveNode.Next != None:
+            PreRemoveNode = RemoveNode.Next
+            RemoveNode = PreRemoveNode.Next
+
+        # Abolish the point in the new last node, effectively removing the last node
+        PreRemoveNode.Next = None
+
+        return 
+
+    def get(self, index): 
+        # Set the reference node to iterate over
+        GetNode = self.rootNode
+
+        if GetNode.Next == None:
+            return "List is empty"
+
+        GetNode = GetNode.Next
+        j = index
+        
+        for i in range(index):
+            GetNode = GetNode.Next
+            j = j - 1
+            if GetNode.Next == None:
+                if j > 0:
+                    return "Index out of bounds"
+
+        return GetNode.Data
+
     
